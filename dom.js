3cbe7a25eb7e7e1b9e1c299edb76718d41be6c2b
Jangwon Suh <jan9won@gmail.com>
2020-12-19 23:14:30 +0900

    update

---
+++ dom.js
@@ -0,0 +1,19 @@
+function classControls() {
+    this.hasClass = function (yourObj, yourClass) {
+        if(!yourObj || typeof yourClass !== 'string') {
+            return false;
+        } else if(yourObj.className && yourObj.className.trim().split(/\s+/gi).indexOf(yourClass) > -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    this.addClass = function (yourObj, yourClass) {
+        var regex = new RegExp(yourClass, "g");
+        yourObj.className += ' ' + yourClass;
+    }
+    this.removeClass = function (yourObj, yourClass) {
+        var regex = new RegExp(yourClass, "g");
+        yourObj.className = yourObj.className.replace(regex, "");
+    }
+}
